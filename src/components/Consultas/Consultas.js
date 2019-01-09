d8a20704630933e0c91df014bdd88542e7339035
Alex Fernandes Romera <af_romera@hotmail.com>
2019-01-08 22:23:56 -0200

    Definindo a Interface da tela de Consultas

--- src/components/Consultas/Consultas.js
+++ src/components/Consultas/Consultas.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
-class Consultas extends Comment {
+class Consultas extends Component {
 
     constructor(props){
         super(props);
@@ -12,12 +12,76 @@ class Consultas extends Comment {
 
     componentDidMount(){
         fetch('http://www.devup.com.br/php/api-dashboard/api/consultas')
-        .then(resultado => resultado.json().then(dados => this.state(dados)));
+        .then(resultado => resultado.json().then(dados => this.setState(dados)));
     }
 
     render(){
         return(
-            <div></div>
+            <div>
+                <h2 className='mt-2'>Consultas</h2>
+
+                <div className='row'>
+
+                    <div className='col'>
+                        <div className='card mt-2'>
+                            <div className='card-header'>
+                                Realizadas
+                            </div>
+                            <table className='table'>
+                                <thead>
+                                    <tr>
+                                        <th>Especialidade</th>
+                                        <th>Quantidade</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.realizadas.map((item,indice) => {
+                                            return(
+                                                <tr key={indice}>
+                                                    <td> {item.especialidade} </td>
+                                                    <td> {item.quantidade} </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <div className='col'>
+                        <div className='card mt-2'>
+                            <div className='card-header'>
+                                Marcadas
+                            </div>
+                            <table className='table'>
+                                <thead>
+                                    <tr>
+                                        <th>Especialidade</th>
+                                        <th>Quantidade</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.marcadas.map((item,indice) => {
+                                            return(
+                                                <tr key={indice}>
+                                                    <td>{item.especialidade}</td>
+                                                    <td>{item.quantidade}</td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         )
     }
 }
+
+export default Consultas
